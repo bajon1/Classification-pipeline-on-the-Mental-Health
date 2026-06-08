db6606825cfa2ad9eafcfccec2d02aa20fde5a50
bajon1 <dmika@student.agh.edu.pl>
2026-06-08 07:50:00 +0200

    model architecture first version done

--- src/model.py
+++ src/model.py
@@ -5,6 +5,19 @@ from torch import nn
 class MLP(nn.Module):
     def __init__(self, input_size, hidden_layers, output_size, dropout_p, activation):
         super().__init__()
+        self.input = nn.Linear(input_size, hidden_layers[0])
+
+        self.hidden_layers = nn.ModuleList([
+            nn.Linear(hidden_layers[i+1], hidden_layers[i]) for i in range(len(hidden_layers))
+        ])
+
+        self.output = nn.Linear(hidden_layers[-1], output_size)
+        self.dropout = nn.Dropout(p=dropout_p)
+        self.activation = nn.ReLU if activation == "relu" else nn.Tanh
 
     def forward(self, x):
-        return x
+        x = self.activation(self.input(x))
+        for layer in self.hidden_layers:
+            x = self.dropout(self.activation(layer(x)))
+
+        return self.output(x)
