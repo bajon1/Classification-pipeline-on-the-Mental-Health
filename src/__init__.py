f275f205ec9849e0c7f7d36a4c27a17824d985ae
bajon1 <dmika@student.agh.edu.pl>
2026-06-09 14:20:46 +0200

    one epoch train and evaluate helper functions completed

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from .model import MLP
 from .dataset import build_fold_dataloaders
 from .train import _train_one_epoch
 from .train import train
-from .train import _evaluate
+from .train import _validate
--- src/train.py
+++ src/train.py
@@ -1,12 +1,50 @@
 import torch
+import numpy as np
 import torch.optim as optim
+import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
 
-def _train_one_epoch(model, optimizer, criterion, train_loader, epoch):
+def _train_one_epoch(model, optimizer, criterion, train_loader, device='cpu'):
     model.train()
-    pass
+    train_loss = 0
 
-def _evaluate(model, val_loader, criterion, epoch):
-    pass
+    for X_batch, y_batch in train_loader:
+        X_batch, y_batch = X_batch.to(device), y_batch.to(device)
 
-def train(model, train_loader, val_loader, optimizer, criterion, fold_idx=0, device='cpu', n_epochs=10):
-    pass
+        optimizer.zero_grad()
+        loss = criterion(model(X_batch), y_batch)
+        loss.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step()
+
+        train_loss += loss.item()*len(X_batch)
+    train_loss /= len(train_loader)
+
+    return train_loss
+
+@torch.no_grad()
+def _validate(model, val_loader, criterion, epoch):
+    model.eval()
+    val_loss = 0
+
+    for X_batch, y_batch in val_loader:
+        loss = criterion(model(X_batch), y_batch)
+        val_loss += loss.item()*len(X_batch)
+    val_loss /= len(val_loader)
+
+    return val_loss
+
+def train(model, train_loader, val_loader, optimizer, criterion, fold_idx=0,
+          device='cpu', n_epochs=100, clip_grad_norm=1.0,  write_model=True,
+          tensorboard_dir="../runs/tensorboard"):
+    train_losses = []
+    val_losses = []
+
+    for epoch in range(n_epochs):
+        train_loss = (_train_one_epoch(model, optimizer, criterion, train_loader))
+        val_loss = (_validate(model, val_loader, criterion, epoch))
+
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+
+    return train_losses, val_losses
